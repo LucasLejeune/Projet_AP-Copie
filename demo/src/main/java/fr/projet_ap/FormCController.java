352604f19fb7b9ef189cb8e74fc8da7c04508118
T1W3R <werquin.timothe@gmail.com>
2022-03-11 09:06:23 +0100

    Modification FormCController du 11/03/22 09:06

--- demo/src/main/java/fr/projet_ap/FormCController.java
+++ demo/src/main/java/fr/projet_ap/FormCController.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
